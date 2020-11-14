82cccb87d0f9953c13b2eb89830ffe42df9c0233
igor <igorsilv@hotmail.com>
2020-11-14 18:10:31 -0300

    Final da Aula 5 - Usando Propriedades

--- conta.py
+++ conta.py
@@ -1,4 +1,3 @@
-
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite=0):
@@ -9,7 +8,7 @@ class Conta:
         self.__limite = limite
 
     def extrato(self):
-        return self.__saldo
+        print("O saldo na conta de {0} é {1}".format(self.__titular, self.__saldo))
 
     def deposita(self, valor):
         self.__saldo += valor
@@ -20,3 +19,23 @@ class Conta:
     def transfere(self, valor, conta_destino):
         self.saca(valor)
         conta_destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def numero(self):
+        return self.__numero
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
